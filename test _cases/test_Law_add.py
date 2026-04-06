5f4309b9cf0c3b17aa94810d9c5bb6c72032094d
Your Name <your_email@example.com>
2026-04-05 21:54:26 -0400

    Update project files

--- test _cases/test_Law_add.py
+++ test _cases/test_Law_add.py
@@ -8,7 +8,7 @@ from import_classified import import_classified_results
 # Path to your JSON file
 json_path = Path("doc_processing_results/A_Digest_of_the_General_Laws_of_Kentucky-1866_results_classified.json")
 
-# Run import ONLY ONCE
+# Run import
 print("Starting single import...")
 import_classified_results(json_path)
 print("Import complete!")
